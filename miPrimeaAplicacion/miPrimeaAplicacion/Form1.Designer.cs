47530e7cb06d7f5a229b80b429b8aace4d7bb07a
Fernando Contreras <fernandorcontreras2006@gmail.com>
2026-09-21 12:36:46 -0600

    Creación de la base de datos y las tablas alumnos, matricula y materias

--- miPrimeaAplicacion/miPrimeaAplicacion/Form1.Designer.cs
+++ miPrimeaAplicacion/miPrimeaAplicacion/Form1.Designer.cs
@@ -27,13 +27,8 @@
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(736, 511);
-            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.ClientSize = new System.Drawing.Size(698, 514);
             this.Name = "Form1";
-            this.Text = "Conversor de Longitud";
-            this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
 
         }
